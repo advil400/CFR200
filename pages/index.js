92f436adbff951bab7d50ba8d99b81c85749ce9c
400 <advil@400.services>
2021-10-14 03:03:55 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import commerce from "../lib/commerce";
+import ProductList from "../components/ProductList";
+
+
+export async function getStaticProps() {
+  const merchant = await commerce.merchants.about();
+  const { data: categories } = await commerce.categories.list();
+  const { data: products } = await commerce.products.list();
+
+  return {
+    props: {
+      merchant,
+      categories,
+      products,
+    },
+  };
+}
+
+export default function IndexPage({ merchant, categories, products }) {
+    return (
+      <React.Fragment>
+        <pre>{JSON.stringify(merchant, null, 2)}</pre>
+        <pre>{JSON.stringify(categories, null, 2)}</pre>
+        <ProductList products={products} />
+      </React.Fragment>
+    );
+  }
